9f402da1003d962bdb8f8210f113c2b8db6d37cc
Kicsi99 <kissnorbi9935@gmail.com>
2016-10-10 08:25:32 +0200

    1 perces időzítő majd a játékhoz ha meglesz végre a textarea :D

---
+++ core/src/com/mygdx/game/Game/Idozito.java
@@ -0,0 +1,45 @@
+package com.mygdx.game.Game;
+
+import java.awt.event.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.swing.*;
+
+/**
+ * Created by Kicsi on 2016. 10. 08..
+ */
+
+public class Idozito {
+
+    SimpleDateFormat sdf = new SimpleDateFormat("mm:ss,S");
+    Timer ido = new Timer(10, new Esemeny());
+    GregorianCalendar gc = new GregorianCalendar();
+    GregorianCalendar nulla = new GregorianCalendar();
+
+    public Idozito() {
+        beallit();
+        GameStage.setText(sdf.format(gc.getTime()));
+        ido.start();
+    }
+
+    void beallit(){
+        gc.set(Calendar.MINUTE, 1);
+        gc.set(Calendar.MILLISECOND, 0);
+        gc.set(Calendar.SECOND, 0);
+        gc.set(Calendar.HOUR_OF_DAY, 0);
+        nulla.set(Calendar.MILLISECOND, 0);
+        nulla.set(Calendar.HOUR_OF_DAY, 0);
+        nulla.set(Calendar.SECOND, 0);
+        nulla.set(Calendar.MINUTE, 0);
+    }
+
+    class Esemeny implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            gc.add(Calendar.MILLISECOND, -10);
+            GameStage.setText(sdf.format(gc.getTime()));
+            if(gc.getTimeInMillis() == nulla.getTimeInMillis()) ido.stop();
+        }
+    }
+}
