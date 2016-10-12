a48fd8e88139cb3e7a5584f3d9a293881efb8f60
mordes <csongorhegedus2@indamail.hu>
2016-10-12 19:30:33 +0200

    Szintekből fölösleges volt külön screen + stage, konkrétan csak a generálás, és az információ más a többi mind ugyan az. Az információ mostantól az InformationStage ill. InformationScreen-ben kapott helyet, és abból nyílik a játék.

--- core/src/com/mygdx/game/Game/InformationStage.java
+++ core/src/com/mygdx/game/Game/InformationStage.java
@@ -1,4 +1,4 @@
-package com.mygdx.game.EasyLevel;
+package com.mygdx.game.Game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.Batch;
@@ -6,39 +6,52 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.mygdx.game.Game.GameScreen;
+import com.mygdx.game.ButtonToText;
+import com.mygdx.game.Menu.MenuStage;
+import com.mygdx.game.Menu.PlayScreen;
+import com.mygdx.game.Menu.PlayStage;
 import com.mygdx.game.MyButton;
 import com.mygdx.game.MyScreen;
 import com.mygdx.game.MyStage;
-import com.mygdx.game.Menu.PlayScreen;
-import com.mygdx.game.ButtonToText;
 
 /**
- * Created by Vince on 2016. 10. 07..
+ * Created by mordes on 2016.10.12..
  */
 
+public class InformationStage extends MyStage{
 
-public class EasyLevelStage extends MyStage {
     private TextButton textButton,textButton2, szovegButton;
 
+    private static String difficultyEasyInformation = "Könnyű szint!\r\nEgyes feladványok 1 vagy 2 műveletet tartalmaznak!\r" +
+            "\nA feladatok nem tartalmaznak zárójelet!\r",
+            difficultyMediumInformation = "Közepes szint!\r\nEgyes feladványok 1, 2 vagy 3 műveletet tartalmaznak!\r" +
+            "\nA feladatok tartalmazhatnak zárójelet!\r",
+            difficultyHardInformation = "Nehéz szint!\r\nEgyes feladványok 2 vagy 3 műveletet tartalmaznak!\r"+
+            "\nA feladatok tartalmazhatnak zárójelet!",
+            currentDifficultyInformation;
 
 
-    public EasyLevelStage(Game game) {
+    public InformationStage(Game game) {
         super(game);
     }
 
-
-    public EasyLevelStage(Viewport viewport, Batch batch, Game game) {
-        super(viewport, batch, game);
-    }
-    public EasyLevelStage(Viewport viewport, Game game) {
+    public InformationStage(Viewport viewport, Game game) {
         super(viewport, game);
     }
 
-
+    public InformationStage(Viewport viewport, Batch batch, Game game) {
+        super(viewport, batch, game);
+    }
 
     @Override
     protected void init() {
+
+        switch (PlayStage.difficulty) {
+            case 1:currentDifficultyInformation = difficultyEasyInformation; break;
+            case 2:currentDifficultyInformation = difficultyMediumInformation; break;
+            case 3:currentDifficultyInformation = difficultyHardInformation; break;
+        }
+
         textButton2 = new MyButton("Teszt indítása!");
         textButton2.addListener(new ClickListener(){
             @Override
@@ -51,7 +64,9 @@ public class EasyLevelStage extends MyStage {
         textButton2.setPosition(MyScreen.WORLD_WIDTH/2 - (textButton2.getWidth()/2),MyScreen.WORLD_HEIGHT * (3/4f));
         addActor(textButton2);
 
-        szovegButton = new ButtonToText("Könnyű szint!\r\nEgyes feladványok 1 vagy 2 műveletet tartalmaznak!\r\nA feladatok nem tartalmaznak zárójelet!\r\nA feladatok megoldására 1 perc áll rendelkezésre!\r\n10 feladatra kell válaszolt adni!\r\nSok sikert!");
+        szovegButton = new ButtonToText(currentDifficultyInformation+
+                "\nA feladatok megoldására 1 perc áll rendelkezésre!\r" +
+                "\n10 feladatra kell válaszolt adni!\r\nSok sikert!");
         szovegButton.setSize(MyScreen.WORLD_WIDTH,MyScreen.WORLD_HEIGHT/2);
         szovegButton.setPosition(MyScreen.WORLD_WIDTH/2 - (szovegButton.getWidth()/2),MyScreen.WORLD_HEIGHT * (2/10f));
         addActor(szovegButton);
@@ -62,6 +77,7 @@ public class EasyLevelStage extends MyStage {
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
                 game.setScreen(new PlayScreen(game));
+
             }
         });
         textButton.setSize(170f,60f);
@@ -70,13 +86,12 @@ public class EasyLevelStage extends MyStage {
     }
 
     @Override
-    public void act() {
-        super.act();
-    }
-
-    @Override
     public void dispose() {
         super.dispose();
     }
-}
 
+    @Override
+    public void act() {
+        super.act();
+    }
+}
