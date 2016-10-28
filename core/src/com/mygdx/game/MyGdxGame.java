6f17bab36084ef703e04d46f0ffaa7aff2e2424e
mordes <csongorhegedus2@indamail.hu>
2016-10-28 18:08:19 +0200

    Update MyGdxGame.java

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -9,7 +9,10 @@ További tartalmak:
 -animáció a betöltő képernyőn
 -zene a menükben, illetve a játék alatt (zene megállító, indító gomb)
 -készítők menüpont
--nehézségi fokozatok (könnyű, közepes, nehéz)
+-nehézségi fokozatok
+ könnyű (-200 és 200 közötti lehet a 6. elem 1 vagy 2 művelet)
+ közepes (-1000 és 1000 közötti lehet a 6. elem 1,2 vagy 3 művelet)
+ nehéz (-10000 és 10000 közötti lehet a 6. elem 2 vagy 3 művelet)
 -saját beviteli mező a számoknak, kilépés a futó játékból a menübe
 -időzítő (minden feladatra 60másodperc)
 */
